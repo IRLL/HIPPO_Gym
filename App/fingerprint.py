4e5333a3b5f234aa8ffb442253b36b4bdd29672a
Nick Nissen <dev@nicknissen.com>
2021-06-17 11:26:11 -0600

    add some print statments to fingerprint.py for now

--- App/fingerprint.py
+++ App/fingerprint.py
@@ -8,12 +8,17 @@ class Fingerprint:
 
     def __init__(self):
         self.checker = pexpect.spawn('wine score2K.exe', encoding='utf-8')
+        print("Checker Init")
 
     def check_xml(self, filename):
         end = '\r\n'
+        print("Checker Started")
         self.checker.expect('Enter*')
+        print("Enter found")
         self.checker.sendline(filename+end)
+        print("Checker Input Given")
         self.checker.expect('Score: ')
         score = self.checker.readline().strip()
+        print("Score: ", score)
         return score
 
