4e483938603080e19ab3163b4e136080015a5208
Mathïs Fédérico <mathfederico@gmail.com>
2021-07-09 16:39:16 +0200

    Added convert_relative_coordinates

--- App/hippo_pygame.py
+++ App/hippo_pygame.py
@@ -2,7 +2,12 @@ from message_handler import MessageHandler
 
 import pygame
 from pygame.event import Event
-from pygame.locals import MOUSEBUTTONDOWN, MOUSEBUTTONUP
+from pygame.locals import MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEMOTION
+
+def convert_relative_coordinates(x_rel, y_rel):
+    screen = pygame.display.get_surface()
+    w, h = screen.get_size()
+    return (int(x_rel * w), int(y_rel * h))
 
 class PyGameMessageHandler(MessageHandler):
 
@@ -13,26 +18,34 @@ class PyGameMessageHandler(MessageHandler):
         event_type = message['info']
 
         if hasattr(self.trial.agent, 'handle_events'):
-            if event_type == 'point clicked':
-                
-                screen = pygame.display.get_surface()
-                w, h = screen.get_size()
 
-                coords = (
-                    int(message['coordinates']['xRel'] * w),
-                    int(message['coordinates']['yRel'] * h)
-                )
+            if event_type == 'point clicked':
+
+                coords = message['coordinates']
+                coords = convert_relative_coordinates(coords['xRel'], coords['yRel'])
 
                 events = [
                     Event(MOUSEBUTTONDOWN, pos=coords, button=1),
                     Event(MOUSEBUTTONUP, pos=coords, button=1)
                 ]
-                
+
                 self.trial.humanAction = self.trial.agent.handle_events(events)
 
             elif event_type == 'mouse motion':
-                pass
+
+                print(message)
+                # raise NotImplementedError
+
+                # pos = (
+                #     int(message['coordinates']['xRel'] * w),
+                #     int(message['coordinates']['yRel'] * h)
+                # )
+
+                # events = [
+                #     Event(MOUSEMOTION, pos=pos, rel=rel)
+                # ]
+                
 
         else:
-            raise NotImplementedError('Agent has no member handle_event')
+            raise NotImplementedError('Agent has no member handle_events')
 
