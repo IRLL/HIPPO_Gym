6f186987fa5fdf56cf65436198a6613e0d77547a
Nick Nissen <nnissen@ualberta.ca>
2021-07-15 17:36:19 -0600

    start packaging scaffold

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='hippo_gym',
+    version='2.0.0a',
+    description='Human Input Parsing Platform for OpenAI Gym',
+    author='Nick Nissen',
+    author_email='nnissen@ualberta.ca',
+    install_requires=[
+        'shortuuid>=1.0.1',
+        'asyncio>=3.4.3',
+        'websockets>=9.1',
+        'numpy>=1.18.5',
+        'Pillow>=8.2.0',
+        'boto3>=1.14.20',
+        'python-dotenv>=0.14.0',
+        'pyYaml>=5.4'
+    ],
+    url='https://hippogym.irll.net',
+    license='MIT',
+    packages=setuptools.find_packages(),
+)
