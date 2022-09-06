7445c3eda8017ee957526ae012707871c90e5c36
Mathïs Fédérico <mathfederico@gmail.com>
2022-09-06 02:36:29 +0200

    :hammer: Refactor message_handler

--- App/message_handlers/message_handler.py
+++ App/message_handlers/message_handler.py
@@ -1,10 +1,31 @@
 from __future__ import annotations
+from enum import Enum
 
 import shortuuid
 from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
     from App.trial import Trial
+
+
+class MessageType(Enum):
+    """Enumeration of message types that the MessageHandler can handle."""
+
+    COMMAND = "command"
+    FRAMERATE_CHANGE = "changeFrameRate"
+    ACTION = "action"
+    INFO = "info"
+    USER_ID = "userId"
+
+
+class Commands(Enum):
+    """Enumeration of commands that the MessageHandler can handle."""
+
+    START = "start"
+    STOP = "stop"
+    RESET = "reset"
+    PAUSE = "pause"
+    REQUEST_UI = "requestui"
 
 
 class MessageHandler:
@@ -18,78 +39,86 @@ class MessageHandler:
         """
         Reads messages sent from websocket, handles commands as priority then actions and infos.
         """
-        if not self.trial.userId and "userId" in message:
-            self.userId = message["userId"] or f"user_{shortuuid.uuid()}"
+        handlers = {
+            MessageType.USER_ID: self.handle_user_id,
+            MessageType.COMMAND: self.handle_command,
+            MessageType.FRAMERATE_CHANGE: self.handle_framerate_change,
+            MessageType.ACTION: self.handle_action,
+            MessageType.INFO: self.handle_user_id,
+        }
+        for message_type in MessageType:
+            if message_type.value in message:
+                msg_type = MessageType(msg_type)
+                handler = handlers[msg_type]
+                handler(message)
+                return  # TODO: Check why we shouln't perform multiple handler
+
+    def handle_command(self, message: dict):
+        """Deals with allowable commands from user."""
+        command = message[MessageType.COMMAND]
+        command = str(command).strip().lower()
+        command = Commands(command)
+        command_effects = {
+            Commands.START: self.trial.resume,
+            Commands.STOP: self.trial.end,
+            Commands.RESET: self.trial.reset,
+            Commands.PAUSE: self.trial.pause,
+            Commands.REQUEST_UI: self.trial.send_ui,
+        }
+        command_effects[command]()
+
+    def handle_user_id(self, message: dict):
+        user_id = message[MessageType.USER_ID]
+        if not self.trial.user_id:
+            user_id = user_id or f"user_{shortuuid.uuid()}"
             self.trial.send_ui()
             self.trial.send_variables()
             self.trial.reset()
             self.trial.send_render()
-        if "command" in message and message["command"]:
-            self.handle_command(str(message["command"]))
-        elif "changeFrameRate" in message and message["changeFrameRate"]:
-            self.handle_framerate_change(message["changeFrameRate"])
-        elif "action" in message and message["action"]:
-            self.handle_action(message["action"])
-        elif "info" in message:
-            self.handle_mouse_event(message)
 
-    def handle_command(self, command: str):
-        """Deals with allowable commands from user."""
-        command = command.strip().lower()
-        if command == "start":
-            self.trial.play = True
-        elif command == "stop":
-            self.trial.end()
-        elif command == "reset":
-            self.trial.reset()
-        elif command == "pause":
-            self.trial.play = False
-        elif command == "requestUI":
-            self.trial.send_ui()
-        return command
-
-    def handle_framerate_change(self, change: str):
+    def handle_framerate_change(self, message: dict):
         """
         Changes the framerate in either increments of step, or to a requested
         value within a minimum and maximum bound.
         """
+        change = str(message[MessageType.FRAMERATE_CHANGE])
         if not self.trial.config.get("allowFrameRateChange"):
             return
 
         step = self.trial.config.get("frameRateStepSize", 5)
-        minFR = self.trial.config.get("minFrameRate", 1)
-        maxFR = self.trial.config.get("maxFrameRate", 90)
+        min_fps = self.trial.config.get("minFrameRate", 1)
+        max_fps = self.trial.config.get("maxFrameRate", 90)
         change = change.strip().lower()
-        if change == "faster" and self.trial.framerate + step < maxFR:
+        if change == "faster" and self.trial.framerate + step < max_fps:
             self.trial.framerate += step
-        elif change == "slower" and self.trial.framerate - step > minFR:
+        elif change == "slower" and self.trial.framerate - step > min_fps:
             self.trial.framerate -= step
         else:
-            try:
-                requested = int(change)
-                if requested > minFR and requested < maxFR:
-                    self.trial.framerate = requested
-            except:
-                pass
+            requested = int(change)
+            if min_fps < requested < max_fps:
+                self.trial.framerate = requested
 
-    def handle_action(self, action: Union[str, int]):
+    def handle_action(self, message: dict):
         """
         Translates action to int and resets action buffer
         """
+        action: Union[str, int] = message[MessageType.ACTION]
+
+        # Directly given action index
         if isinstance(action, int):
             self.trial.human_action = action
             return
 
+        # Action as str to interpret
         action = action.strip().lower()
-        actionSpace = self.trial.config.get("actionSpace")
-        if action in actionSpace:
-            action = actionSpace.index(action)
+        action_space = self.trial.config.get("actionSpace")
+        if action in action_space:
+            action = action_space.index(action)
         else:
             action = self.trial.config.get("defaultAction")
         self.trial.human_action = action
 
-    def handle_mouse_event(self, message: dict):
+    def handle_info(self, message: dict):
         """
-        Translates mouse events into a custom behavior
+        Translates info into a custom behavior
         """
-        pass
