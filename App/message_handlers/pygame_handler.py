f5a8a8cc6784f0a3e2781c256b08317558baa756
Mathïs Fédérico <mathfederico@gmail.com>
2022-09-06 02:43:01 +0200

    :hammer: Refactor PyGameMessageHandler

--- App/message_handlers/pygame_handler.py
+++ App/message_handlers/pygame_handler.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import pygame
 from pygame.event import Event
 from pygame.locals import MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEMOTION
@@ -5,37 +6,47 @@ from pygame.locals import MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEMOTION
 from App.message_handlers import MessageHandler
 
 
-def convert_relative_coordinates(x_rel, y_rel):
+def rel_to_abs_coords(x_rel: float, y_rel: float) -> Tuple[int, int]:
+    """Convert relative to absolute coordinates on the pygame window.
+
+    Args:
+        x_rel (float): Relative horizontal coordinates.
+        y_rel (float): Relative vertical coordinates.
+
+    Returns:
+        Tuple[int, int]: Absolute pixel coordinates on the pygame window.
+    """
     screen = pygame.display.get_surface()
     w, h = screen.get_size()
     return (int(x_rel * w), int(y_rel * h))
 
 
 class PyGameMessageHandler(MessageHandler):
-    def handle_info(self, event_type: dict):
+    """
+    A MessageHandler design to send incomming messages as pygame events.
+    """
+
+    def handle_info(self, message: dict):
         """
         Send pygames events to the trial agent.
         """
 
-        if hasattr(self.trial.agent, "handle_events"):
-
-            pos = message["pos"]
-            pos = convert_relative_coordinates(pos["xRel"], pos["yRel"])
-
-            if event_type == "mouse up":
-                events = [Event(MOUSEBUTTONUP, pos=pos, button=1)]
-            elif event_type == "mouse down":
-                events = [Event(MOUSEBUTTONDOWN, pos=pos, button=1)]
-            elif event_type == "mouse move":
-                rel = message["rel"]
-                rel = convert_relative_coordinates(
-                    rel["xRelMovement"], rel["yRelMovement"]
-                )
-                events = [Event(MOUSEMOTION, pos=pos, rel=rel)]
-
-            action = self.trial.agent.handle_events(events)
-            if action is not None:
-                self.handle_action(action)
-
-        else:
+        if not hasattr(self.trial.agent, "handle_events"):
             raise NotImplementedError(f"Agent cannot handle mouse_event: {message}")
+
+        event_type = message["info"]
+        pos = message["pos"]
+        pos = rel_to_abs_coords(pos["xRel"], pos["yRel"])
+
+        if event_type == "mouse up":
+            events = [Event(MOUSEBUTTONUP, pos=pos, button=1)]
+        elif event_type == "mouse down":
+            events = [Event(MOUSEBUTTONDOWN, pos=pos, button=1)]
+        elif event_type == "mouse move":
+            rel = message["rel"]
+            rel = rel_to_abs_coords(rel["xRelMovement"], rel["yRelMovement"])
+            events = [Event(MOUSEMOTION, pos=pos, rel=rel)]
+
+        action = self.trial.agent.handle_events(events)
+        if action is not None:
+            self.handle_action(action)
