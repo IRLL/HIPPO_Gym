99a9365c10b04244d525e5baad9343811c1656dc
Mathïs Fédérico <mathfederico@gmail.com>
2022-09-12 00:23:32 +0200

    :fire: Remove unused handle_config

--- src/hippogym/__main__.py
+++ src/hippogym/__main__.py
@@ -1,23 +1,7 @@
-import os
 import hippogym
 
-from hippogym.handle_config import handle_config
-
-
-def main():
+def main() -> None:
     print(hippogym.__doc__)
-    config_path = "config.yml"
-    stepfiles_path = "StepFiles"
-    project_config_path = ".project_config.yml"
-    trial_config_path = ".trial_config.yml"
-
-    if not os.path.exists(project_config_path) or not os.path.exists(trial_config_path):
-        handle_config(
-            config_path=config_path,
-            stepfiles_path=stepfiles_path,
-            project_config_path=project_config_path,
-            trial_config_path=trial_config_path,
-        )
 
 
 if __name__ == "__main__":
