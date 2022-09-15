deeeefea61055450a0a481229923862a3a74717f
Mathïs Fédérico <mathfederico@gmail.com>
2022-09-15 16:41:55 +0200

    :umbrella: Add unit test for UIElement update

---
+++ tests/unit/ui_elements/test_ui_element.py
@@ -0,0 +1,35 @@
+import pytest
+import pytest_check as check
+from pytest_mock import MockerFixture
+
+from hippogym.ui_elements.ui_element import UIElement
+
+
+class DummyUIElement(UIElement):
+    def __init__(self, *args, **kwargs) -> None:
+        self.dummy_attr = kwargs.pop("dummy_attr", None)
+        super().__init__(*args, **kwargs)
+
+    def dict(self) -> dict:
+        return {"Dummy": {"dummy_attr": self.dummy_attr}}
+
+
+class TestUIElement:
+    """UIElement"""
+
+    @pytest.fixture(autouse=True)
+    def setup(self, mocker: MockerFixture):
+        self.queue = mocker.MagicMock()
+
+    def test_update_to_value(self):
+        """should update attributes to None if they are given"""
+        ui_element = DummyUIElement(self.queue)
+        ui_element.update(dummy_attr=2)
+        check.equal(ui_element.dummy_attr, 2)
+
+    def test_update_to_none(self):
+        """should update attributes to None if they are given"""
+        ui_element = DummyUIElement(self.queue, dummy_attr=2)
+        check.equal(ui_element.dummy_attr, 2)
+        ui_element.update(dummy_attr=None)
+        check.is_none(ui_element.dummy_attr)
