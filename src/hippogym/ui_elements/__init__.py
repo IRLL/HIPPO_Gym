6d77d9486f6dba81e57ba4fce865c8cd4eedd2a2
Mathïs Fédérico <mathfederico@gmail.com>
2022-09-15 09:42:12 +0200

    :tada: Add shortcut to ui_elements

---
+++ src/hippogym/ui_elements/__init__.py
@@ -0,0 +1,13 @@
+from hippogym.ui_elements.game_window import GameWindow
+from hippogym.ui_elements.info_panel import InfoPanel
+from hippogym.ui_elements.grid import Grid
+from hippogym.ui_elements.text_box import TextBox
+from hippogym.ui_elements.control_panel import *
+
+__all__ = [
+    "ControlPanel",
+    "GameWindow",
+    "InfoPanel",
+    "Grid",
+    "TextBox",
+]
