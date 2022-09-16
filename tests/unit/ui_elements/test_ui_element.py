b20c37768b98ff069193929acfd08c866ba4d645
Mathïs Fédérico <mathfederico@gmail.com>
2022-09-16 12:38:55 +0200

    :umbrella: Fix tests

--- tests/unit/ui_elements/test_ui_element.py
+++ tests/unit/ui_elements/test_ui_element.py
@@ -19,17 +19,18 @@ class TestUIElement:
 
     @pytest.fixture(autouse=True)
     def setup(self, mocker: MockerFixture):
-        self.queue = mocker.MagicMock()
+        self.message_handler = mocker.MagicMock()
+        self.out_q = mocker.MagicMock()
 
     def test_update_to_value(self):
-        """should update attributes to None if they are given"""
-        ui_element = DummyUIElement(self.queue)
+        """should update attributes to given value"""
+        ui_element = DummyUIElement(self.message_handler, self.out_q)
         ui_element.update(dummy_attr=2)
         check.equal(ui_element.dummy_attr, 2)
 
     def test_update_to_none(self):
         """should update attributes to None if they are given"""
-        ui_element = DummyUIElement(self.queue, dummy_attr=2)
+        ui_element = DummyUIElement(self.message_handler, self.out_q, dummy_attr=2)
         check.equal(ui_element.dummy_attr, 2)
         ui_element.update(dummy_attr=None)
         check.is_none(ui_element.dummy_attr)
