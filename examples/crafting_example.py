550a04d7e2a1c46f5ddd7419bc2e9c4290ec5da2
Mathïs Fédérico <mathfederico@gmail.com>
2022-10-17 03:12:14 +0200

    :tada: First kinda working version Need a frontend fix to get relative coordinates instead of absolute

--- examples/crafting_example.py
+++ examples/crafting_example.py
@@ -7,7 +7,7 @@ pip install git+https://github.com/IRLL/Crafting.git#egg=crafting
 """
 
 import logging
-from typing import Tuple
+from typing import Optional, Tuple
 
 import pygame
 from pygame.event import Event
@@ -18,7 +18,6 @@ from crafting.task import TaskObtainItem
 from crafting.render.render import get_human_action
 
 from hippogym import HippoGym, Agent
-from hippogym.ui_elements import InfoPanel, ControlPanel, standard_controls
 from hippogym.trialsteps import GymStep
 from hippogym.queue_handler import check_queue
 from hippogym.event_handler import EventsQueues
@@ -29,32 +28,51 @@ LOGGER = logging.getLogger(__name__)
 
 class HumanCraftingAgent(Agent):
     def __init__(self, observation_space=None, action_space=None) -> None:
-        self.trialstep: "GymStep" = None
+        self.trialstep: Optional["GymStep"] = None
         super().__init__(observation_space, action_space)
+
+    def message_to_event(self, message) -> Optional[Event]:
+        window_size = self.trialstep.render_window.size
+        if "MOUSEBUTTONUP" in message:
+            event_type = MOUSEBUTTONUP
+            pos, buttons, button = message["MOUSEBUTTONUP"]
+            rel = None
+        elif "MOUSEBUTTONDOWN" in message:
+            event_type = MOUSEBUTTONDOWN
+            pos, buttons, button = message["MOUSEBUTTONDOWN"]
+            rel = None
+        elif "MOUSEMOTION" in message:
+            event_type = MOUSEMOTION
+            pos, rel, buttons, button = message["MOUSEMOTION"]
+        else:
+            return
+
+        pos_relative = (pos["x"] / window_size[0], pos["y"] / window_size[1])
+        pos_abs = rel_to_abs_coords(*pos_relative)
+
+        if rel is not None:
+            rel_relative = (
+                rel["xMovement"] / window_size[0],
+                rel["yMovement"] / window_size[1],
+            )
+            rel_abs = rel_to_abs_coords(*rel_relative)
+            return Event(event_type, pos=pos_abs, rel=rel_abs, button=button)
+        return Event(event_type, pos=pos_abs, button=1)
 
     def act(self, observation):
         for message in check_queue(self.trialstep.queues[EventsQueues.WINDOW]):
 
-            # Get mouse event position
-            event_type = message["info"]
-            pos = message["pos"]
-            pos = rel_to_abs_coords(pos["xRel"], pos["yRel"])
-
             # Create fake pygame mouse event
-            if event_type == "mouse up":
-                events = [Event(MOUSEBUTTONUP, pos=pos, button=1)]
-            elif event_type == "mouse down":
-                events = [Event(MOUSEBUTTONDOWN, pos=pos, button=1)]
-            elif event_type == "mouse move":
-                rel = message["rel"]
-                rel = rel_to_abs_coords(rel["xRelMovement"], rel["yRelMovement"])
-                events = [Event(MOUSEMOTION, pos=pos, rel=rel)]
+            fake_event = self.message_to_event(message)
+
+            if fake_event is None:
+                continue
 
             # Get action from crafting rendering
             env: CraftingEnv = self.trialstep.env
             action = get_human_action(
                 env,
-                additional_events=events,
+                additional_events=[fake_event],
                 can_be_none=True,
                 **env.render_variables,
             )
@@ -65,47 +83,22 @@ class HumanCraftingAgent(Agent):
 
 class MineCraftingStep(GymStep):
     def __init__(self, agent):
-        self.info_panel = InfoPanel(text="Use your mouse to play the game")
-        self.control_panel = ControlPanel(
-            buttons=standard_controls,
-            keys=True,
-        )
         self.score = 0
         env = MineCraftingEnv(verbose=1, max_step=50, render_mode="rgb_array")
-        env.add_task(
-            TaskObtainItem(env.world, env.world.item_from_name["enchanting_table"])
-        )
-        super().__init__(
-            env,
-            agent,
-            ui_elements=[self.info_panel, self.control_panel],
-        )
+        task = TaskObtainItem(env.world, env.world.item_from_name["enchanting_table"])
+        env.add_task(task)
+        super().__init__(env, agent)
 
-    def step(
-        self,
-        observation,
-        action,
-        new_observation,
-        reward: float,
-        done: bool,
-        info: dict,
-    ) -> None:
+    def run(self) -> None:
+        self.env.reset()
+        self.env.render()
+        self.render_window.set_size(*pygame_screen_size())
+        return super().run()
 
-        if done:
-            self.info_panel.update(key_value={"Score": reward})
-            self.score = 0
-            return
 
-        observation_msg = ", ".join([f"{x:.2f}" for x in new_observation])
-        observation_msg = f"[{observation_msg}]"
-        self.score += reward
-        self.info_panel.update(
-            key_value={
-                "Score": self.score,
-                "Reward": reward,
-                "Observation": observation_msg,
-            },
-        )
+def pygame_screen_size() -> Tuple[int, int]:
+    screen = pygame.display.get_surface()
+    return screen.get_size()
 
 
 def rel_to_abs_coords(x_rel: float, y_rel: float) -> Tuple[int, int]:
@@ -118,9 +111,8 @@ def rel_to_abs_coords(x_rel: float, y_rel: float) -> Tuple[int, int]:
     Returns:
         Tuple[int, int]: Absolute pixel coordinates on the pygame window.
     """
-    screen = pygame.display.get_surface()
-    width, height = screen.get_size()
-    return (int(x_rel * width), int(y_rel * height))
+    width, height = pygame_screen_size()
+    return int(x_rel * width), int(y_rel * height)
 
 
 def build_experiment() -> HippoGym:
