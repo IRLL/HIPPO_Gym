a62bc9e9bc3a62f527aef1bb1c5b3e382a45f710
Mathïs Fédérico <mathfederico@gmail.com>
2022-10-24 01:09:33 +0200

    :bug: Fix crafting example

--- examples/crafting_example.py
+++ examples/crafting_example.py
@@ -6,6 +6,7 @@ pip install git+https://github.com/IRLL/Crafting.git#egg=crafting
 
 """
 
+from copy import copy
 import logging
 from typing import Optional, Tuple
 
@@ -27,21 +28,22 @@ LOGGER = logging.getLogger(__name__)
 class HumanCraftingAgent(Agent):
     def __init__(self, observation_space=None, action_space=None) -> None:
         self.trialstep: Optional["GymStep"] = None
+        self.action = None
         super().__init__(observation_space, action_space)
 
-    def message_to_event(self, message) -> Optional[Event]:
+    def message_to_event(self, event_type, event_data) -> Optional[Event]:
         window_size = self.trialstep.render_window.size
-        if "MOUSEBUTTONUP" in message:
-            event_type = MOUSEBUTTONUP
-            pos, buttons, button = message["MOUSEBUTTONUP"]
+        if event_type == "MOUSEBUTTONUP":
+            pygame_event_type = MOUSEBUTTONUP
+            pos, buttons, button = event_data
             rel = None
-        elif "MOUSEBUTTONDOWN" in message:
-            event_type = MOUSEBUTTONDOWN
-            pos, buttons, button = message["MOUSEBUTTONDOWN"]
+        elif event_type == "MOUSEBUTTONDOWN":
+            pygame_event_type = MOUSEBUTTONDOWN
+            pos, buttons, button = event_data
             rel = None
-        elif "MOUSEMOTION" in message:
-            event_type = MOUSEMOTION
-            pos, rel, buttons, button = message["MOUSEMOTION"]
+        elif event_type == "MOUSEMOTION":
+            pygame_event_type = MOUSEMOTION
+            pos, rel, buttons, button = event_data
         else:
             return
 
@@ -54,28 +56,33 @@ class HumanCraftingAgent(Agent):
                 rel["yMovement"] / window_size[1],
             )
             rel_abs = rel_to_abs_coords(*rel_relative)
-            return Event(event_type, pos=pos_abs, rel=rel_abs, button=button)
-        return Event(event_type, pos=pos_abs, button=1)
+            return Event(pygame_event_type, pos=pos_abs, rel=rel_abs, button=button)
+        return Event(pygame_event_type, pos=pos_abs, button=1)
+
+    def on_mouse_event(self, event_type: "MouseEvent", event_data):
+        # Create fake pygame mouse event
+        fake_event = self.message_to_event(event_type, event_data)
+
+        if fake_event is None:
+            return
+
+        # Get action from crafting rendering
+        env: CraftingEnv = self.trialstep.env
+        action = get_human_action(
+            env,
+            additional_events=[fake_event],
+            can_be_none=True,
+            **env.render_variables,
+        )
+        if action:
+            self.action = env.action(*action)
 
     def act(self, observation):
-        pass
-        # # Create fake pygame mouse event
-        # fake_event = self.message_to_event(message)
-
-        # if fake_event is None:
-        #     continue
-
-        # # Get action from crafting rendering
-        # env: CraftingEnv = self.trialstep.env
-        # action = get_human_action(
-        #     env,
-        #     additional_events=[fake_event],
-        #     can_be_none=True,
-        #     **env.render_variables,
-        # )
-        # if action:
-        #     action = env.action(*action)
-        # return action
+        if self.action is not None:
+            action = copy(self.action)
+            self.action = None
+            return action
+        return self.action
 
 
 class MineCraftingStep(GymStep):
