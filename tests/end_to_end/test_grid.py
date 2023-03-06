74b5f2bf215860c86b306937a6858050acd95726
Mathïs Fédérico <mathfederico@gmail.com>
2023-03-06 18:25:54 +0100

    :umbrella: Refactor & Fix end_to_end tests

--- tests/end_to_end/test_grid.py
+++ tests/end_to_end/test_grid.py
@@ -1,47 +1,8 @@
-import json
-
-import pytest
-import pytest_check as check
-
 from examples.grid import build_experiment
-from hippogym.hippogym import HippoGym
-from websockets.client import connect
-import asyncio
-
-from tests import server_client_interaction, get_uri
+from tests.end_to_end.custom_checks import check_ui_elements
 
 
-def test_grid(unused_tcp_port: int):
+def test_grid_ui_elements(unused_tcp_port: int):
     hippo = build_experiment()
-
-    async def fake_user_connect(uri: str, user_id: str):
-        """Connect send user_id then close connextion"""
-        async with connect(uri) as websocket:
-            # Connects
-            await websocket.send(json.dumps({"userId": user_id}))
-            message = await websocket.recv()
-
-            ui_elements_messages = [message]
-            assert user_id in hippo.trials, "Trial did not start."
-
-            expected_ui_elements = ("InfoPanel", "Grid", "ControlPanel")
-
-            for _ in expected_ui_elements[1:]:
-                ui_elements_messages.append(await websocket.recv())
-
-            for expected_ui_element in expected_ui_elements:
-                check.is_true(
-                    any(expected_ui_element in msg for msg in ui_elements_messages),
-                    f"Expected UIElement was not found: {expected_ui_element}",
-                )
-
-    user_id = "fake_user"
-    host = "localhost"
-    port = unused_tcp_port
-    uri = get_uri(host, port)
-    asyncio.run(
-        server_client_interaction(
-            server_coroutine=hippo.start_server(host, port),
-            client_coroutine=fake_user_connect(uri, user_id),
-        )
-    )
+    expected_ui_elements = ("InfoPanel", "Grid", "ControlPanel")
+    check_ui_elements(hippo, expected_ui_elements, unused_tcp_port)
--- tests/end_to_end/test_minigrid.py
+++ tests/end_to_end/test_minigrid.py
@@ -1,50 +1,9 @@
-import json
-
-import pytest
-import pytest_check as check
-
 from examples.minigrid_example import build_experiment
-from websockets.client import connect
-import asyncio
-
-from tests import server_client_interaction, get_uri
+from tests.end_to_end.custom_checks import check_ui_elements
 
 
-def test_minigrid(unused_tcp_port: int):
+def test_minigrid_ui_elements(unused_tcp_port: int):
     """minigrid example should start a trial on connexion and give expected UIElements"""
     hippo = build_experiment()
-
-    async def fake_user_connect(uri: str, user_id: str):
-        """Connect send user_id then close connextion"""
-        async with connect(uri) as websocket:
-            # Connects
-            await websocket.send(json.dumps({"userId": user_id}))
-
-            message = await websocket.recv()
-
-            ui_elements_messages = [message]
-            assert user_id in hippo.trials, "Trial did not start."
-
-            expected_ui_elements = ("InfoPanel", "GameWindow", "ControlPanel")
-
-            for _ in expected_ui_elements[1:]:
-                message = await asyncio.wait_for(websocket.recv(), 1)
-                ui_elements_messages.append(message)
-
-            for expected_ui_element in expected_ui_elements:
-                check.is_true(
-                    any(expected_ui_element in msg for msg in ui_elements_messages),
-                    f"Expected UIElement was not found: {expected_ui_element}",
-                )
-
-    user_id = "fake_user"
-    host = "localhost"
-    port = unused_tcp_port
-    uri = get_uri(host, port)
-    asyncio.run(
-        server_client_interaction(
-            server_coroutine=hippo.start_server(host, port),
-            client_coroutine=fake_user_connect(uri, user_id),
-        ),
-        debug=True,
-    )
+    expected_ui_elements = ("InfoPanel", "GameWindow", "ControlPanel")
+    check_ui_elements(hippo, expected_ui_elements, unused_tcp_port)
