f1c54f36c428c725e6468b7e2f105d94feba3bf8
Mathïs Fédérico <60117466+MathisFederico@users.noreply.github.com>
2023-04-24 13:01:16 +0200

    Delete test_text_box.py

--- tests/end_to_end/test_text_box.py
+++
@@ -1,9 +0,0 @@
-from examples.text_box import build_experiment
-
-from tests.end_to_end.custom_checks import check_ui_elements
-
-
-def test_text_box_ui_elements(unused_tcp_port: int):
-    hippo = build_experiment()
-    expected_ui_elements = ("InfoPanel", "TextBox", "ControlPanel", "GameWindow")
-    check_ui_elements(hippo, expected_ui_elements, unused_tcp_port)
