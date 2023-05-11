a3cbaab4ae2c4db1b6cde0fd37771c1e19d79551
vmaoued <vmaoued@gmail.com>
2023-05-11 13:26:22 -0600

    Lunar Lander conditional

--- src/hippogym/trialsteps/gymstep.py
+++ src/hippogym/trialsteps/gymstep.py
@@ -40,7 +40,13 @@ class GymStep(InteractiveStep):
             ui_elements.append(self.render_window)
         super().__init__(ui_elements)
         if isinstance(env, str):
-            env = gym.make(env, render_mode="rgb_array", **kwargs)
+            print(self.user_id, self)
+            if "LunarLander" in str(self):
+                print("test1")
+                env = gym.make(env, render_mode="rgb_array", **kwargs)
+            else:
+                print("test2")
+                env = gym.make(env, **kwargs)
         self.env = env
         self.agent = agent
         self.agent.set_spaces(self.env.observation_space, self.env.action_space)
@@ -144,7 +150,12 @@ class GymStep(InteractiveStep):
         return observation, info
 
     def send_render(self):
-        rgb_array = self.env.render()
+        if "LunarLander" in str(self):
+            print("check1")
+            rgb_array = self.env.render()
+        else:
+            print("check1")
+            rgb_array = self.env.render(mode="rgb_array")
         try:
             rgb_array = np.array(rgb_array)
         except:
