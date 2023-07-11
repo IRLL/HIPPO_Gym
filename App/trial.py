5c502f6050c43a9a4a7f928a0662aa22ce5275ac
Mohamed Al-Nassirat <malnassi@ualberta.ca>
2023-07-11 15:52:23 -0600

    new_cognitive_tutor should now be working, saving data is still not fully ready yet but everything else is ready. Logic behind websockets has been reworked so now all you need to run currently is trial.py, no need for communicator.py anymore.

--- App/trial.py
+++ App/trial.py
@@ -2,12 +2,16 @@ import string
 import numpy, json, shortuuid, time, base64, yaml, logging, os, xml.etree.ElementTree as ET, errno
 import _pickle as cPickle
 from xml.dom import minidom
-from PIL import Image
+
 from io import BytesIO
 #from agent import Agent # this is the Agent/Environment compo provided by the researcher
 
 # Get the score change from score_change
 from predict import get_score_change
+import asyncio
+import websockets
+import json
+
 
 # for dummy score
 import random
@@ -21,13 +25,13 @@ def load_config():
 
 class Trial():
 
-    def __init__(self, pipe):
+    def __init__(self, connection_url):
         self.config = load_config()
         self.trialData = None
         self.data = None
         self.count = 1
-
-        self.pipe = pipe
+        self.connection_url = connection_url
+        self.websocket = None
         # self.humanAction = 0
         self.episode = 0
         self.done = False
@@ -46,6 +50,25 @@ class Trial():
 
         self.start()
         self.run()
+
+    async def connect(self):
+        retries = 0
+        while retries < 5:
+            try:
+                async with websockets.connect(self.connection_url) as ws:
+                    self.websocket = ws
+                    print("Connected to WebSocket address")
+                    await self.run()
+
+            except Exception as e:
+                print(f"Failed to connect: {e}")
+                retries += 1
+                await asyncio.sleep(1)  # wait a bit before retrying
+
+
+
+
+
 
     def start(self):
         '''
@@ -66,24 +89,19 @@ class Trial():
         #self.agent = Agent()
         #self.agent.start(self.config.get('game'))
 
-    def run(self):
+    async def run(self):
         '''
-        This is the main event controlling function for a Trial. 
+        This is the main event controlling function for a Trial.
         It handles the render-step loop
         '''
         self.create_file()
         while not self.done:
-            message = self.check_message()
+            message = await self.check_message()
             if message:
-                self.handle_message(message)
-            # if self.play:
-                # self.take_step()
-                # render = self.get_render()
-                # self.send_render(render)
-                # self.play = False
-            # time.sleep(1/self.framerate)
+                await self.handle_message(message)
 
-    def reset(self):
+
+    async def reset(self):
         '''
         Resets the OpenAI gym environment to start a new episode.
         By default this function will create a new log file for every
@@ -92,14 +110,15 @@ class Trial():
         self.create_file.
         '''
         if self.check_trial_done():
-            print("IN HERE TO END")
+            print("check_trial_done successful")
             self.end()
         else:
             #self.agent.reset()
             if self.outfile:
+                print("self.outfile successful")
                 self.outfile.close()
-                if self.config.get('s3upload'):
-                    self.pipe.send({'upload':{'projectId':self.projectId ,'userId':self.userId,'file':self.filename,'path':self.path, 'bucket': self.config.get('bucket')}})
+                # if self.config.get('s3upload'):
+                #     self.pipe.send({'upload':{'projectId':self.projectId ,'userId':self.userId,'file':self.filename,'path':self.path, 'bucket': self.config.get('bucket')}})
             self.create_file()
             self.episode += 1
             print('self.episode', self.episode)
@@ -112,157 +131,108 @@ class Trial():
         print('maxEpisodes', self.config.get('maxEpisodes', 20))
         return self.episode >= self.config.get('maxEpisodes', 20)
 
-    def end(self):
+    async def end(self):
         '''
         Closes the environment through the agent, closes any remaining outfile
         and sends the 'done' message to the websocket pipe. If logging the 
         whole trial memory in self.record, uncomment the call to self.save_record()
         to write the record to file before closing.
         '''
-        self.pipe.send('done')
+        
+        await self.websocket.send(json.dumps('done'))
         #self.agent.close()
         if self.config.get('dataFile') == 'trial':
             self.save_record()
         if self.outfile:
             self.outfile.close()
-            self.pipe.send({'upload':{'projectId':self.projectId,'userId':self.userId,'file':self.filename,'path':self.path}})
+            await self.websocket.send({'upload':{'projectId':self.projectId,'userId':self.userId,'file':self.filename,'path':self.path}})
         self.play = False
         self.done = True
 
-    def check_message(self):
+    async def check_message(self):
         '''
         Checks pipe for messages from websocket, tries to parse message from
-        json. Retruns message or error message if unable to parse json.
+        json. Returns message or error message if unable to parse json.
         Expects some poorly formatted or incomplete messages.
         '''
-        if self.pipe.poll():
-            message = self.pipe.recv()
+        if self.websocket:
+            message = await self.websocket.recv()
             try:
                 message = json.loads(message)
+                print("check_message function recieved: ", message)
             except:
                 message = {'error': 'unable to parse message', 'frameId': self.frameId}
             return message
         return None
 
-    def handle_message(self, message:dict):
+
+    async def handle_message(self, message:dict):
         '''
         Reads messages sent from websocket, handles commands as priority then 
         actions. Logs entire message in self.nextEntry
         '''
-        print(message)
+        print("handle_message function: ", message)
         if not self.userId and 'userId' in message:
             self.userId = message['userId'] or f'user_{shortuuid.uuid()}'
-            # get a randomly selected trial 
-            # with open('data/more_trials.json') as json_file:
-            #     selectedTrial =  0 #random.randint(0, 4)
-            #     allTrials = json.load(json_file)
-            #     print('all trials')
-            #     print(allTrials)
-            #     self.trialData = allTrials[str(selectedTrial)]
-            #     print('self.trialData')
-            #     print(self.trialData)
+            print("self.userID is now = ", self.userId)
             with open('./data/trialData.json') as json_file:
                 self.trialData = json.load(json_file)
-            self.send_ui()
+            await self.send_ui()
         if 'command' in message and message['command']:
-            self.handle_command(message)
+            await self.handle_command(message)
         elif 'save' in message and message['save']:
             self.nextEntry = message['save']
-            print(self.nextEntry)
+            print("self.nextEntry: ",self.nextEntry)
             self.save_entry()
 
 
-    def handle_command(self, message):
+    async def handle_command(self, message):
         '''
         Deals with allowable commands from user. To add other functionality
         add commands.
         '''
         command = message['command'].strip().lower()
+        print("handle_command function: ", command)
         if command == 'get next':
             if self.count > 1:
                 try:
-                    self.pipe.send(json.dumps({'VALUES': self.data['qs'][message['info']]}))
+                    print("sending.. :" , {'action': 'command','VALUES': self.data['qs'][message['info']]})
+                    await self.websocket.send(json.dumps({'action': 'command','VALUES': self.data['qs'][message['info']]}))
                 except:
                     raise TypeError("Render Dictionary is not JSON serializable")
         elif command == 'new game' and self.count < 43:
             self.count+=1
-            self.send_ui()
+            print("self.count has been incremented: ", self.count)
+            await self.send_ui()
+            await self.reset()
             #if (self.count != 3 and self.passedHeader != 0) or (self.count != 23 and self.passedHeader!=1):
-            print('self.passedHeader', self.passedHeader)
-            print('self.count', self.count)
-            print('self.passedHeader != 0', self.passedHeader != 0)
-            print('self.count != 3', self.count != 3)
-            print('self.count != 23 ', self.count != 23 )
-            print('self.passedHeader!=1', self.passedHeader!=1)
+            # print('self.passedHeader', self.passedHeader)
+            # print('self.count', self.count)
+            # print('self.passedHeader != 0', self.passedHeader != 0)
+            # print('self.count != 3', self.count != 3)
+            # print('self.count != 23 ', self.count != 23 )
+            # print('self.passedHeader!=1', self.passedHeader!=1)
             #if (self.count != 3 and self.passedHeader != 0) or (self.count != 23 and self.passedHeader!=1):
                 #print("resetting env....")
-            self.reset()
+            
         elif command == 'resume' and self.count < 43:
-            self.send_ui()
+            await self.send_ui()
         elif self.count >= 43:
             print("IN HEREEEEE")
             self.end()
 
-    # def handle_action(self, action:str):
-    #     '''
-    #     Translates action to int and resets action buffer if action !=0
-    #     '''
-    #     action = action.strip().lower()
-    #     actionSpace = self.config.get('actionSpace')
-    #     if action in actionSpace:
-    #         actionCode = actionSpace.index(action)
-    #     else:
-    #         actionCode = 0
-    #     self.humanAction = actionCode
-
-    # def update_entry(self, update_dict:dict):
-    #     '''
-    #     Adds a generic dictionary to the self.nextEntry dictionary.
-    #     '''
-    #     self.nextEntry.update(update_dict)
-
-    # def get_render(self):
-    #     '''
-    #     Calls the Agent/Environment render function which must return a npArray.
-    #     Translates the npArray into a jpeg image and then base64 encodes the 
-    #     image for transmission in json message.
-    #     '''
-    #     render = self.agent.render()
-
-    #     # Get the image name from the rendered image path
-    #     # from "Images/<filename>.bmp" get "<filename>"
-    #     self.imagename = render[7: -4]
-
-    #     try:
-    #         img = Image.open(render)
-    #         fp = BytesIO()
-    #         img.save(fp, 'BMP') # Changes filetype to be BMP
-    #         frame = base64.b64encode(fp.getvalue()).decode('utf-8')
-    #         fp.close()
-    #     except:
-    #         raise TypeError("Render failed. Is env.render('rgb_array') being called\
-    #                         With the correct arguement?")
-    #     self.frameId += 1
-    #     return {'frame': frame, 'frameId': self.frameId}
-
-    # def send_render(self, render:dict):
-    #     '''
-    #     Attempts to send render message to websocket
-    #     '''
-    #     try: 
-    #         self.pipe.send(json.dumps(render))
-    #     except:
-    #         raise TypeError("Render Dictionary is not JSON serializable")
-
-    def send_ui(self):
+    async def send_ui(self):
         if (self.count == 3 and self.passedHeader == 0) or (self.count == 23 and self.passedHeader == 1):
             print("header sending..")
             if(self.count == 3):
                 message = "This next section is the training section"
+                print("message: ", message)
             else:
                 message = "This next section is the testing section"
+                print("message: ", message)
             try:
-                self.pipe.send(json.dumps({'HEADER': message}))
+                print("sending.. :" , {'action': 'UI','HEADER': message})
+                await self.websocket.send(json.dumps({'action': 'UI','HEADER': message}))
                 self.passedHeader +=1
             except:
                 raise TypeError("Render Dictionary is not JSON serializable")
@@ -279,11 +249,13 @@ class Trial():
                 ctest = "CTEST"
 
             if(self.count == 1 or self.count == 43):
+                print("self.count is 1 or 43")
                 print('self.count', self.count)
                 self.data = self.trialData[str(self.count)]
                 print('self.data', self.data)
-                try:
-                    self.pipe.send(json.dumps({'UI': self.data, "CTEST": "CTEST"}))
+                try:          
+                    print("sending... ",{"action": 'UI', 'UI': self.data, "CTEST": "CTEST"} )
+                    await self.websocket.send(json.dumps({"action": 'UI', 'UI': self.data, "CTEST": "CTEST"}))
                 except:
                     raise TypeError("Render Dictionary is not JSON serializable")
             else:
@@ -292,71 +264,18 @@ class Trial():
                 #print('self.data', self.data)
                 with open('data/increasing_prs.json') as json_file:
                     data = json.load(json_file)
-                    #print('type of data', type(data))
+                    
                     for group in data:
-                        #print('group', group['trial_id'])
+                        print('group', group['trial_id'])
                         if group['trial_id'] == self.data:
                             print('USING TRIAL', group['trial_id'])
                             self.data = group
                 try:
-                    self.pipe.send(json.dumps({'UI': self.data['stateRewards'], 'OPT_ACT': self.data['opt_act'], 'FEEDBACK': feedback, "CTEST": ctest}))
+                    print("sending.. :" , {'action': 'UI','UI': self.data['stateRewards'], 'OPT_ACT': self.data['opt_act'], 'FEEDBACK': feedback, "CTEST": ctest})
+                    await self.websocket.send(json.dumps({'action': 'UI','UI': self.data['stateRewards'], 'OPT_ACT': self.data['opt_act'], 'FEEDBACK': feedback, "CTEST": ctest}))
                 except:
                     raise TypeError("Render Dictionary is not JSON serializable")
 
-    # def send_instructions(self, instructions):
-    #     if not instructions:
-    #         instructions = self.config.get('instructions', None)
-    #     if instructions:
-    #         try: 
-    #             self.pipe.send(json.dumps({'Instructions': instructions}))
-    #         except:
-    #             return
-
-    # def send_fingerprint_config(self):
-    #     max_score = self.config.get('maxScore')
-    #     min_minutiae = self.config.get('minMinutiae')
-    #     self.pipe.send(json.dumps({'Fingerprint': self.fingerprint, 'MaxScore': max_score, 'MinMinutiae': min_minutiae}))
-
-    # def get_score(self):
-    #     # Get the score from the windows machine
-
-    #     # For now, just sleep for 2 seconds and return a random score
-    #     time.sleep(2)
-    #     max_score = self.config.get('maxScore')
-    #     return random.randint(0, max_score)
-
-    # def send_score(self, score):
-    #     try:
-    #         self.pipe.send(json.dumps({'Score': score}))
-    #     except:
-    #         return
-
-    # def send_expert(self):
-    #     try:
-    #         #Try to load in the first xml
-    #         expert_1 = xmlToArray(f'Fingerprints/{self.imagename}.FingerNet.xml')
-    #         #Try to load in the second xml
-    #         expert_2 = xmlToArray(f'Fingerprints/{self.imagename}.MinutiaNet.xml')
-    #         #Try to send it to front end
-    #         self.pipe.send(json.dumps({'ExpertMarks1': expert_1, 'ExpertMarks2': expert_2}))
-    #     except:
-    #         return
-
-    # def take_step(self):
-    #     '''
-    #     Expects a dictionary return with all the values that should be recorded.
-    #     Records return and saves all memory associated with this setp.
-    #     Checks for DONE from Agent/Env
-    #     '''
-    #     envState = self.agent.step(self.humanAction)
-    #     self.update_entry(envState)
-    #     self.save_entry()
-    #     if envState['done']:
-    #         self.reset()
-    #     else:
-    #         score = self.agent.get_score(self.xmlFilename)
-    #         self.send_score(score)
-    #     self.play = True
 
     def save_entry(self):
         '''
@@ -406,72 +325,11 @@ class Trial():
             print('saved')
         self.filename = filename
         self.path = path
+        print(f"created a file at: {filename}")
 
-    # def handle_minutiae(self, command:str, minutiaList:list):
-    #     '''
-    #     Creates a new XML file in the in the XML folder
-    #     using the given minutia list
-    #     '''
-    #     XMLstring = self.createXML(minutiaList)
+async def main():
+    trial = Trial('wss://x4v1m0bphh.execute-api.ca-central-1.amazonaws.com/production?connection_type=backend')
+    await trial.connect()
+    await trial.run()
 
-    #     user = 'Nadeen'
-    #     filename = f'{self.imagename}'
-    #     path = f'XML/{filename}.xml'
-    #     self.xmlFilename = path
-
-    #     try:
-    #         XMLfile = open(path, 'x')
-    #         XMLfile.write(XMLstring)
-    #         XMLfile.close()
-    #     except OSError as e:
-    #         # No such directory
-    #         if e.errno == errno.ENOENT:
-    #             os.makedirs('XML') # create "XML" directory
-    #         # File name already exists
-    #         elif e.errno == errno.EEXIST:
-    #             files = [file for file in os.listdir('XML') if filename in file]
-    #             i = 1
-    #             while filename + str(i) +".xml" in files:
-    #                 i += 1
-    #             path = f'XML/{filename}{str(i)}.xml'
-
-    #         XMLfile = open(path, 'x')
-    #         XMLfile.write(XMLstring)
-    #         XMLfile.close()
-
-    #     if command == "getFeedback":
-    #         score_change = get_score_change(path)
-    #         self.pipe.send(json.dumps({'ScoreChange': score_change}))
-
-    #     self.play = True
-
-    # def createXML(self, minutiae:list):
-    #     '''
-    #     Creates and returns an XML string with the following structure:
-    #         <MinutiaeList>
-    #             <Minutia X="313" Y="381" Angle="342.0" Type="End" />
-    #             ...
-    #         </MinutiaeList>
-    #     '''
-    #     minutiaeList = ET.Element('MinutiaeList')
-
-    #     for minutia in minutiae:
-    #         ET.SubElement(minutiaeList, 'Minutia',
-    #             {'X': str(minutia['x']).split('.')[0],
-    #             'Y': str(minutia['y']).split('.')[0],
-    #             'Angle': str(minutia['orientation']),
-    #             'Type': str(minutia['type'])})
-
-    #     tab_length = "" # defining tab length to be 4 spaces
-    #     rough_string = ET.tostring(minutiaeList)
-    #     reparsed = minidom.parseString(rough_string)
-
-    #     return reparsed.toprettyxml(indent=tab_length)
-
-# def xmlToArray(path):
-#     tree = ET.parse(path)
-#     root = tree.getroot()
-#     minutiae = []
-#     for child in root:
-#         minutiae.append({'x': int(child.attrib['X']), 'y': int(child.attrib['Y']), 'orientation': float(child.attrib['Angle'].replace(',', '.'))})
-#     return minutiae
+asyncio.run(main())
